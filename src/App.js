73ff005457f3766079f5f47790bc98572af6fa5a
Jhovan Espinal <jhovan1028@gmail.com>
2021-04-13 18:22:36 -0500

    Feature SignIn a SignOut

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React,{UseEffect, useRef, useState} from "react";
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB7Ihzj4FAWrZBN3N2CaXNuunkxLDoQdu8",
+  authDomain: "sofkachat-team-8c4e9.firebaseapp.com",
+  projectId: "sofkachat-team-8c4e9",
+  storageBucket: "sofkachat-team-8c4e9.appspot.com",
+  messagingSenderId: "641682756235",
+  appId: "1:641682756235:web:aa7ba464072b53784d3a8e"
+});
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+
 
 function App() {
-  return (
+const [user] = useAuthState(auth);
+
+return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>
+    <h1>SofkaU Chat</h1>
+    <SignOut/>
+    </header>
+    <section>
+      {user ? <ChatRoom /> : <SignIn/>}
+    </section>
+    
     </div>
   );
 }
+
+function ChatRoom(){
+ return <p>chat</p>
+}
+
+function SignIn(){
+const signInWithGoogle = () =>{
+
+const provider = new firebase.auth.GoogleAuthProvider();
+auth.signInWithPopup(provider); 
+}
+
+  return <button onClick={signInWithGoogle}>Sign in with google</button>
+ }
+
+ function SignOut(){
+  return auth.currentUser && (
+    <button onClick={() => {
+      auth.signOut();
+    }}>SignOut</button>
+  )
+
+ }
+
+ 
 
 export default App;
